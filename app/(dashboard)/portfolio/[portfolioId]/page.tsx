65b613a507b2a2ada9ee93ff75f6480dc3eede29
Emmanuelbreezy <emmabreezyumeh@gmail.com>
2025-04-05 23:44:00 +0100

    Refactor sidebar layout in portfolio page for improved styling and structure

--- app/(dashboard)/portfolio/[portfolioId]/page.tsx
+++ app/(dashboard)/portfolio/[portfolioId]/page.tsx
@@ -1,5 +1,5 @@
-import { SidebarTrigger } from "@/components/ui/sidebar";
 import React from "react";
+import { SidebarTrigger } from "@/components/ui/sidebar";
 import PdfRenderer from "../../_components/PdfRenderer";
 
 const Page = () => {
@@ -7,14 +7,12 @@ const Page = () => {
     <div className="flex-1 bg-white justify-between flex flex-col h-screen">
       <div className="mx-auto w-full max-w-8xl grow lg:flex xl:px-2">
         {/* Left sidebar & main wrapper */}
-        <div className="flex-1 xl:flex">
-          <div className="px-4 py-6 sm:px-6 lg:pl-8 xl:flex-1 xl:pl-6">
-            <div>
-              <SidebarTrigger />
-            </div>
-            {/* Main area */}
-            {/* <PortfolioWrapper isSubscribed={plan.isSubscribed} fileId={file.id} /> */}
+        <div className="flex-1 xl:flex px-2 py-2">
+          <div>
+            <SidebarTrigger />
           </div>
+          {/* Main area */}
+          {/* <PortfolioWrapper isSubscribed={plan.isSubscribed} fileId={file.id} /> */}
         </div>
 
         <div className="shrink-1 flex-[0.65] border-t border-gray-200 lg:w-96 lg:border-l lg:border-t-0">
